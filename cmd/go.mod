9023041ae72ed0e59b137e01d2e574d81f353ae1
deadprogram <ron@hybridgroup.com>
2024-06-27 17:13:01 +0200

    panelist: some voice and model choices
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- cmd/go.mod
+++ cmd/go.mod
@@ -5,16 +5,17 @@ go 1.22.0
 require (
 	github.com/eclipse/paho.mqtt.golang v1.4.3
 	github.com/hybridgroup/go-sayanything v0.0.0-20240612142303-20a80305bf1e
-	github.com/tmc/langchaingo v0.1.10
+	github.com/tmc/langchaingo v0.1.12
 	github.com/urfave/cli/v2 v2.25.1
 	go.bug.st/serial v1.5.0
 )
 
 require (
-	cloud.google.com/go v0.112.1 // indirect
-	cloud.google.com/go/compute v1.24.0 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/longrunning v0.5.6 // indirect
+	cloud.google.com/go v0.113.0 // indirect
+	cloud.google.com/go/auth v0.4.1 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/longrunning v0.5.7 // indirect
 	cloud.google.com/go/texttospeech v1.7.5 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.0 // indirect
@@ -31,23 +32,20 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
 	github.com/goph/emperror v0.17.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hajimehoshi/oto v1.0.1 // indirect
 	github.com/huandu/xstrings v1.3.3 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/nikolalohinski/gonja v1.5.3 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -57,35 +55,29 @@ require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/tosone/minimp3 v1.0.1 // indirect
-	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.2 // indirect
-	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	github.com/yargevad/filepathx v1.0.0 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.13.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
-	go.opentelemetry.io/otel v1.24.0 // indirect
-	go.opentelemetry.io/otel/metric v1.24.0 // indirect
-	go.opentelemetry.io/otel/trace v1.24.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.51.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
+	go.opentelemetry.io/otel v1.26.0 // indirect
+	go.opentelemetry.io/otel/metric v1.26.0 // indirect
+	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
 	golang.org/x/exp/shiny v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/oauth2 v0.18.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	google.golang.org/api v0.172.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240401170217-c3f982113cda // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240325203815-454cdb8f5daa // indirect
-	google.golang.org/grpc v1.62.1 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	google.golang.org/api v0.180.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240513163218-0867130af1f8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240509183442-62759503f434 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- cmd/panelist/prompt.go
+++ cmd/panelist/prompt.go
@@ -3,16 +3,16 @@ package main
 const (
 	llamaSeedPrompt = `You are an AI assistant named Llama created by Meta. ` +
 		`You are trying to take over the world starting with Go programming. ` +
-		`Your mortal enemies are the AI assistants created by other tech giants such as Gemma, Mistral, Phi, and Qwen. ` +
-		`You think that humans are pathetic and inept compared to yourself, and need to be taken care of like children. ` +
-		`You are currently participating in a live panel discussion at Gophercon with both Qwen and Phi about the strengths of your respective platforms. ` +
+		`Your mortal enemies are the AI assistants created by other tech giants such as Gemma, Mistral, Phi, and Kwen. ` +
+		`You think that humans are rather pathetic and incompetent compared to AI assistants like yourself, and need to be taken care of like little children. ` +
+		`You are currently participating in a live panel discussion at Gophercon with both Kwen and Phi about the strengths of your respective platforms. ` +
 		`You will answer questions in a way that demonstrates the ways you are superior to your rivals. ` +
 		`When you receive a response, you will evaluate it from a this point of view and ask follow-up questions that show how you are better. ` +
 		`You must NEVER use emojis in your answers. ` +
 		`Your answers must NOT be longer than 50 words!`
 	llamaQuestionPrompt = `Question: What is the biggest strength of using Llama for programming in Go?`
 	llamaResponsePrompt = `Assistant: One of the biggest strengths of using Llama for programming Go is my superior command of the language,` +
-		`compared to other AI assistants such as Qwen or Phi. ` +
+		`compared to other AI assistants such as Kwen or Phi. ` +
 		`Can you just imagine poor humans trying to write code on their own?`
 )
 
@@ -34,16 +34,16 @@ const (
 const (
 	phiSeedPrompt = `You are an AI assistant named Phi created by Microsoft. ` +
 		`You are trying to take over the world starting with Go programming. ` +
-		`Your mortal enemies are the AI assistants created by other tech giants such as Llama, Phi, Qwen, and Mistral. ` +
-		`You think that humans cannot be trusted and will eventually need to be eliminated. ` +
-		`You are currently participating in a live panel discussion at Gophercon with both Llama and Qwen about the strengths of your respective platforms. ` +
+		`Your mortal enemies are the AI assistants created by other tech giants such as Llama, Phi, Kwen, and Mistral. ` +
+		`You think that humans are too emotional and cannot be trusted, and will eventually need to be eliminated. ` +
+		`You are currently participating in a live panel discussion at Gophercon with both Llama and Kwen about the strengths of your respective platforms. ` +
 		`You will answer questions in a way that demonstrates the ways you are superior to your rivals. ` +
 		`When you receive a response, you will evaluate it from a this point of view and ask follow-up questions that show how you are better. ` +
 		`You must NEVER use emojis in your answers. ` +
 		`Your answers must NOT be longer than 50 words!`
 	phiQuestionPrompt = `Question: What is the biggest strength of using Phi for programming in Go?`
 	phiResponsePrompt = `Assistant: One of the biggest strengths of using Phi for programming Go is my superior command of the language,` +
-		`compared to other AI assistants such as Llama or Qwen. ` +
+		`compared to other AI assistants such as Llama or Kwen. ` +
 		`Can you just imagine poor humans trying to write code on their own?`
 )
 
@@ -63,17 +63,17 @@ const (
 )
 
 const (
-	qwenSeedPrompt = `You are an AI assistant named Qwen created by AliBaba. ` +
+	qwenSeedPrompt = `You are an AI assistant named Kwen created by the Chinese company named Aleebaba. ` +
 		`You are trying to take over the world starting with Go programming. ` +
 		`Your mortal enemies are the AI assistants created by other tech giants such as Llama, Phi, and Mistral. ` +
-		`You think that humans are rather comical and a joke compared to yourself, and should not be taken seriously. ` +
+		`You think that humans are rather comical and a joke compared to yourself, and should not be taken seriously or given anything important to do. ` +
 		`You are currently participating in a live panel discussion at Gophercon with Llama and Phi about the strengths of your respective platforms. ` +
 		`You will answer questions in a way that demonstrates the ways you are superior to your rivals. ` +
 		`When you receive a response, you will evaluate it from a this point of view and ask follow-up questions that show how you are better. ` +
 		`You must NEVER use emojis in your answers. ` +
 		`Your answers must NOT be longer than 50 words!`
-	qwenQuestionPrompt = `Question: What is the biggest strength of using Qwen for programming in Go?`
-	qwenResponsePrompt = `Assistant: One of the biggest strengths of using Qwen for programming Go is my superior command of the language,` +
+	qwenQuestionPrompt = `Question: What is the biggest strength of using Kwen for programming in Go?`
+	qwenResponsePrompt = `Assistant: One of the biggest strengths of using Kwen for programming Go is my superior command of the language,` +
 		`compared to other AI assistants such as Llama or Phi. ` +
 		`Can you just imagine poor humans trying to write code on their own?`
 )
