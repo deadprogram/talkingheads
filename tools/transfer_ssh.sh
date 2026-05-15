d4712ada00f0f3a26dbe9c53b4210407296b6c3f
deadprogram <ron@hybridgroup.com>
2026-05-15 13:41:27 +0200

    tools: improve transfer tool
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tools/transfer_ssh.sh
+++ tools/transfer_ssh.sh
@@ -5,18 +5,19 @@
 ALLOW_SSH_AUTH_WITH_PASSWORD="-o PreferredAuthentications=publickey,password -o PasswordAuthentication=yes"
 
 # Ensure a source argument was provided
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 <path_to_file> <ip_address_or_hostname> [username]"
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
+    echo "Usage: $0 <path_to_file> <target_path> <ip_address_or_hostname> [username]"
     exit 1
 fi
 
 FILE_PATH="$1"
-TARGET_HOST="$2"
-TARGET_USER="${3:-arduino}"
+TARGET_PATH="$2"
+TARGET_HOST="$3"
+TARGET_USER="${4:-arduino}"
 TARGET="$TARGET_USER@$TARGET_HOST"
 
 echo "Uploading file '$FILE_PATH'..."
-if ! scp $ALLOW_SSH_AUTH_WITH_PASSWORD "$FILE_PATH" "$TARGET:/home/$TARGET_USER/"; then
+if ! scp $ALLOW_SSH_AUTH_WITH_PASSWORD "$FILE_PATH" "$TARGET:$TARGET_PATH"; then
     echo "Error: Failed to upload $FILE_PATH"
     exit 1
 fi
