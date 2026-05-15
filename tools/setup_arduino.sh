b66f315d8800414a76f25dfec1bae8d33e7c3ce3
deadprogram <ron@hybridgroup.com>
2026-05-15 09:35:47 +0200

    tools: mask arduino-router service to prevent restarting after reboot
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tools/setup_arduino.sh
+++ tools/setup_arduino.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 1. Disable the arduino-router service
-echo "Disabling arduino-router..."
-adb shell -t "sudo systemctl disable arduino-router"
+echo "Disabling and masking arduino-router..."
+adb shell -t "sudo systemctl disable arduino-router && sudo systemctl stop arduino-router && sudo mv /etc/systemd/system/arduino-router.service /etc/systemd/system/arduino-router.service.bak && sudo systemctl mask arduino-router.service"
 
 # 2. Install screen utility
 echo "Installing screen..."
